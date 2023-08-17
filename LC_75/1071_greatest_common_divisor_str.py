f4f8936c7a492124caa6bedac83e5d2c31292be3
Thomas Dolan <tdolan@berkeley.edu>
2023-08-17 13:52:18 -0700

    Finds gcd combos and checks the highest count

--- LC_75/1071_greatest_common_divisor_str.py
+++ LC_75/1071_greatest_common_divisor_str.py
@@ -1,26 +1,38 @@
-import itertools
+
 def gcdOfStrings(str1, str2):
     str_dict = {}
     str_dict_pop = []
-    gcd_candidates = []
+    gcd_candidates = ''
     gcd_max = ''
+    #Find max length
     if len(str1) >= len(str2):
         maxstr = str1
     else:
         maxstr = str2
+    #Find max count of characters
     for i in maxstr:
         str_dict[i] = str1.count(i)
+    #Remove if not >= max
     for key, value in str_dict.items():
          if value < max(str_dict.values()):
             str_dict_pop.append(key)
     for key in str_dict_pop:
             str_dict.pop(key)
-    gcd_candidates = combinations(str_dict.values(), len(str_dict))
-    return str_dict
+    #Get building combination
+    gcd_list = []
 
-        
-
-
-    
+    keys = list(str_dict.keys())
+    for i in range(len(keys)):
+        temp_str = ''
+        for j in range(i + 1):
+            temp_str += keys[j]
+        gcd_list.append(temp_str)
+    # Check highest GCD
+    gcd_count = 0
+    for gcd in gcd_list:
+        if str1.count(gcd) >= gcd_count:
+            gcd_count = str1.count(gcd)
+            gcd_max = gcd
+    return gcd_max
 
 print(gcdOfStrings("ABABABC", "AB"))
